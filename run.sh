df3dce546a861c02887cd4567b32e49c0154dd4e
Jerome White <jerome@thewex.net>
2024-06-07 06:38:20 +0000

    Use with new message semantics

--- run.sh
+++ run.sh
@@ -11,16 +11,26 @@ source $ROOT/venv/bin/activate
 logs=$HOME/etc/dalgo/logs
 suffix=.json
 
-find $logs -name "*$suffix" \
-    | while read; do
-    out=$ROOT/summary/`realpath --relative-to=$logs $REPLY`
+for l in $logs/*; do
+    case `basename $l` in
+	airbyte) s_prompt=$ROOT/prompts/system/airbyte ;;
+	prefect) s_prompt=$ROOT/prompts/system/dbt ;;
+	*) continue ;;
+    esac
 
-    o_path=`dirname $out`
-    o_name=`basename --suffix=$suffix $out`
+    find $l -name "*$suffix" \
+	| while read; do
+	name=`realpath --relative-to=$logs $REPLY`
+	for f in $ROOT/prompts/user-flows/*; do
+	    o_path=$ROOT/summary/$name/`basename --suffix=.json $l`
+	    o_name=`basename $f`.json.gz
 
-    mkdir --parents $o_path
-    python log-iterator.py \
-	   --log-file $REPLY \
-	   --user-prompt $ROOT/prompts/user-flow.txt \
-	   --system-prompt $ROOT/prompts/system-enhanced.txt > $o_path/$o_name
+	    mkdir --parents $o_path
+	    python log-chat.py \
+		   --log-file $REPLY \
+		   --user-flow $f \
+		   --system-prompt $s_prompt \
+		| gzip --to-stdout > $o_path/$o_name
+	done
+    done
 done
