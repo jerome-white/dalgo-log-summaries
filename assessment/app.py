87025f2671193dda9013bebb5879251ce4869fc0
Jerome White <jerome@thewex.net>
2024-06-10 23:22:35 +0530

    Flask is usable

--- assessment/app.py
+++ assessment/app.py
@@ -1,72 +1,54 @@
 import os
-# import csv
+import csv
 import gzip
 import json
-import string
 import random
 import functools as ft
 from pathlib import Path
-from dataclasses import dataclass
-# from tempfile import NamedTemporaryFile
+from datetime import datetime
+from tempfile import NamedTemporaryFile
 
+import markdown
 import flask as fl
 import pandas as pd
-import markdown
 from flask_httpauth import HTTPBasicAuth
 
-from mylib import Logger
+# from mylib import Logger
 
 app = fl.Flask(__name__)
 auth = HTTPBasicAuth()
 
 @ft.cache
-def whitespacing():
-    ws = {
-        ' ': ' ',
-        '\t': '&nbsp;' * 5,
-    }
-    for i in string.whitespace:
-        ws.setdefault(i, '<br>')
-
-    return ws
-
-def to_html(text):
-    for i in whitespacing().items():
-        text = text.replace(*i)
-
-    return text
-
-@ft.cache
-def dropdown(name):
+def dropdown(prompt):
+    opts = []
     options = {
         '': 'Select an option',
-        1: 'Incorrect',
+	1: 'Incorrect',
         2: 'Decent',
         3: 'Correct',
         4: 'Unsure',
     }
 
-    opts = []
     for (k, v) in options.items():
         extra = '' if k else ' selected disabled hidden'
-        opts.append(f'<option value="{k}"{extra}>{v}</option>')
+        opts.append(f'<option value="{v.lower()}"{extra}>{v}</option>')
 
     return '\n'.join([
-        f'<select name="{name}">',
+        f'<select name="s_{prompt}">',
         *opts,
         '</select>',
     ])
 
 def responses(data):
-    drop = dropdown(data['log'])
-    prompt = 'prompt'
+    p_key = 'prompt'
 
-    for i in data['dialogue']:
-        response = markdown.markdown(i['response'])
+    for (i, d) in enumerate(data['dialogue']):
+        prompt = d[p_key]
+        response = markdown.markdown(d['response'])
         yield {
-            prompt: i[prompt],
+            p_key: prompt,
             'response': response,
-            'judgement': drop,
+            'judgement': dropdown(i),
         }
 
 @ft.cache
@@ -82,13 +64,27 @@ def load(summary):
 def environ():
     keys = (
         'logs',
-        'summaries',
+        'output',
         'storage',
+        'summaries',
     )
 
     for i in keys:
         value = os.getenv('DALGO_{}'.format(i.upper()))
         yield (i, Path(value))
+
+def q_args(q_string):
+    data = load(q_string['summary']).get('dialogue')
+    static = {
+        'date': datetime.now().strftime('%c'),
+        'log': q_string['log'],
+    }
+
+    for (k, v) in q_string.items():
+        if k.startswith('s_'):
+            (_, key) = k.split('_')
+            prompt = data[int(key)]['prompt']
+            yield dict(static, prompt=prompt, judgement=v)
 
 #
 #
@@ -106,13 +102,29 @@ def verify_password(username, password):
 @auth.login_required
 def index():
     dalgo_vars = dict(environ())
-    summary_data = load(random.choice(summaries(dalgo_vars['summaries'])))
+
+    summary_file = random.choice(summaries(dalgo_vars['summaries']))
+    summary_data = load(summary_file)
     event = Path(summary_data['log']).relative_to(dalgo_vars['storage'])
     log_data = dalgo_vars['logs'].joinpath(event)
+
+    if fl.request.args:
+        writer = None
+        with NamedTemporaryFile(mode='w',
+                                suffix='.csv',
+                                prefix='',
+                                dir=dalgo_vars['output'],
+                                delete=False) as fp:
+            for i in q_args(fl.request.args):
+                if writer is None:
+                    writer = csv.DictWriter(fp, fieldnames=i)
+                    writer.writeheader()
+                writer.writerow(i)
 
     return fl.render_template(
         'base.html',
         name=event,
+        summary=summary_file,
         log=pd.read_json(log_data).to_html(),
         responses=responses(summary_data),
     )
